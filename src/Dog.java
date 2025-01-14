1f3c5d1ddbea43eab6a615bb30b164a55d47b510
Shawn Nguru <nguru.shawn@student.greenriver.edu>
2025-01-14 14:20:54 -0600

    Dog Class added

---
+++ src/Dog.java
@@ -0,0 +1,36 @@
+/**
+ * The Dog class represents a dog and provides its ASCII art representation.
+ * This class implements the Animal interface and overrides the getAnimalArt 
+ * and toString methods.
+ * 
+ * dog art sourced from https://www.asciiart.eu/animals/birds-water
+ */
+class Dog implements Animal {
+    /**
+     * Returns the ASCII art representation of the dog.
+     *
+     * @return A string containing the ASCII art of the dog.
+     */
+    @Override
+    public String getAnimalArt() 
+    {
+//By Maija Haavisto https://www.asciiart.eu/animals/dogs
+
+return    "        \\   .\n" +
+          "         \\  ..^____/\n" +
+          "            `-. ___ )\n" +
+          "              ||   ||\n";
+    }
+  
+    /**
+     * Returns the name of the animal ("Dog").
+     * This method overrides the toString method to return the name of the dog.
+     *
+     * @return The string "dog" representing the name of the animal.
+     */
+    @Override
+    public String toString() {
+      return "dog";
+    }
+  }
+  
