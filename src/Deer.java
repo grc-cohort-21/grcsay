2d2cdd556619c052c3252cc85ccb760b08153516
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-01-14 11:48:39 -0800

    Add Deer to src

---
+++ src/Deer.java
@@ -0,0 +1,19 @@
+class Deer implements Animal{
+    
+    @Override
+  public String getAnimalArt() {
+      return 
+        "     \\   (             )\n" +
+        "      \\    `--(_   _)--\'\n" +
+        "       \\       Y-Y\n" +
+        "        \\     /@@ \\\n" +
+        "         \\   /     \\\n" +
+        "             `--\'.  \\             ,\n" +
+        "                 |   `.__________/)";
+  }
+
+  @Override
+  public String toString() {
+    return "deer";
+  }
+}
